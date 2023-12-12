73afe8b382f85fd78284ce22bf4b129b3c579f84
Stanislau Marudau <marudov.stas@gmail.com>
2023-12-12 15:35:38 +0100

    Connected supabse to the frontend

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,4 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://tgyskavgqrywxynmnijk.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRneXNrYXZncXJ5d3h5bm1uaWprIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDIzODYzMzEsImV4cCI6MjAxNzk2MjMzMX0.vPltsIpQtDlDpWP7UEAuuCODB28Ql-X86bI_YWS25oU';
 const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase;
