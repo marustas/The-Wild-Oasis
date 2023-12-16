87d15edfee04e636c9278010cd269af5eae138e6
Stanislau Marudau <marudov.stas@gmail.com>
2023-12-16 14:19:46 +0100

    Implemente the fetching of the app settings

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,21 +1,56 @@
-import Form from ', ../.. /ui/Form';
-import FormRow from '../../ui/FormRow';
-import Input from '../../ui/Input';
+import { useSettings } from "./useSettings";
+
+import Form from ", ../.. /ui/Form";
+import FormRow from "../../ui/FormRow";
+import Input from "../../ui/Input";
+import Spinner from "../../ui/Spinner";
 
 function UpdateSettingsForm() {
+  const {
+    isLoading,
+    settings: {
+      minBookingLength,
+      maxBookingLength,
+      maxGuestsPerBooking,
+      breakfastPrice,
+    } = {},
+  } = useSettings();
+
+  if (isLoading) return <Spinner />;
+
   return (
     <Form>
-      <FormRow label='Minimum nights/booking'>
-        <Input type='number' id='min-nights' />
+      <FormRow label="Minimum nights/booking">
+        <Input
+          disabled={isLoading}
+          type="number"
+          id="min-nights"
+          defaultValue={minBookingLength}
+        />
       </FormRow>
-      <FormRow label='Maximum nights/booking'>
-        <Input type='number' id='max-nights' />
+      <FormRow label="Maximum nights/booking">
+        <Input
+          disabled={isLoading}
+          type="number"
+          id="max-nights"
+          defaultValue={maxBookingLength}
+        />
       </FormRow>
-      <FormRow label='Maximum guests/booking'>
-        <Input type='number' id='max-guests' />
+      <FormRow label="Maximum guests/booking">
+        <Input
+          disabled={isLoading}
+          type="number"
+          id="max-guests"
+          defaultValue={maxGuestsPerBooking}
+        />
       </FormRow>
-      <FormRow label='Breakfast price'>
-        <Input type='number' id='breakfast-price' />
+      <FormRow label="Breakfast price">
+        <Input
+          disabled={isLoading}
+          type="number"
+          id="breakfast-price"
+          defaultValue={breakfastPrice}
+        />
       </FormRow>
     </Form>
   );
