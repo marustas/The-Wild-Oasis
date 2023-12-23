5263e5e6b17adf5036f940d809c5dfd8c77a4e2a
Stanislau Marudau <marudov.stas@gmail.com>
2023-12-23 13:04:44 +0200

    Started building a signup form.

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -10,7 +10,7 @@ export function useLogin() {
         mutationFn: ({ email, password }) => loginApi({ email, password }),
         onSuccess: (user) => {
             navigate('/dashboard');
-            queryClient.setQueriesData(['user'], user)
+            queryClient.setQueryData(['user'], user.user)
         },
         onError: () => { toast.error('There was an error logging in') }
     })
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,7 +1,12 @@
 import Heading from "../ui/Heading";
-
+import SignupForm from "../features/authentication/SignupForm";
 function NewUsers() {
-  return <Heading as="h1">Create a new user</Heading>;
+  return (
+    <>
+      <Heading as="h1">Create a new user</Heading>
+      <SignupForm></SignupForm>
+    </>
+  );
 }
 
 export default NewUsers;
