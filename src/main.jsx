8768337afffdba42af47d74384e453dfe6aec2c1
Stanislau Marudau <marudov.stas@gmail.com>
2023-12-24 17:16:29 +0200

    Implemented error boundary.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import ErrorFallback from "./ui/ErrorFallback";
+
+import { ErrorBoundary } from "react-error-boundary";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+      onReset={() => window.location.replace("/")}
+    ></ErrorBoundary>
     <App />
   </React.StrictMode>
 );
--- src/ui/ErrorFallback.jsx
+++ src/ui/ErrorFallback.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import Heading from "./Heading";
+import GlobalStyles from "../styles/GlobalStyles";
+import Button from "./Button";
 
 const StyledErrorFallback = styled.main`
   height: 100vh;
@@ -29,3 +32,23 @@ const Box = styled.div`
     color: var(--color-grey-500);
   }
 `;
+
+const ErrorFallback = ({ error, resetErrorBoundary }) => {
+  return (
+    <>
+      <GlobalStyles>
+        <StyledErrorFallback>
+          <Box>
+            <Heading as="h1">Something went wrong.</Heading>
+            <p>{error.message}</p>
+            <Button onClick={resetErrorBoundary} size="large">
+              Try again
+            </Button>
+          </Box>
+        </StyledErrorFallback>
+      </GlobalStyles>
+    </>
+  );
+};
+
+export default ErrorFallback;
